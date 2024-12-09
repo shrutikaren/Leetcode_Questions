bc008b390063a779c2c6edee8f8a576a2d1ec58c
Shruti Kaur <ich524@usask.ca>
2024-12-08 18:32:15 -0600

    adding the output from python into the output_python.txt file but each of the element is in fact decimals instead of integer

--- Pascal_Triangle/result.py
+++ Pascal_Triangle/result.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+import numpy as np 
+
+def generate(number): 
+    newlist = np.zeros((number, number))
+    
+    for i in range(number): 
+        for j in range(i): 
+            if (j == 0 or j == i):
+                newlist[i][j] = 1
+            else:
+                newlist[i][j] = newlist[i][j-1] + newlist[i-1][j-1]
+
+    # newlist would have a square pattern where the parts we don't want is a 0
+    # most likely. We want a triangle shape in our shaping system so that our 
+    # list comprehensions keep having that increasing kind of pattern 
+
+    result_output = [list(eachrow[:i+1]) for i, eachrow in enumerate(newlist)]
+
+    return result_output
+
+result_output = generate(5)
+print(result_output)
