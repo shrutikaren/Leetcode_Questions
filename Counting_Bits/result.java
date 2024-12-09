0e2080c761e2b281b7e24eb7bf484ed1b08b2cc4
Shruti Kaur <ich524@usask.ca>
2024-12-09 10:08:14 -0600

    I was having my value set as n instead of i so it kept repeating a specific number in the list. now i fixed it so it is able to produce the correct values for me

--- Counting_Bits/result.java
+++ Counting_Bits/result.java
@@ -1,24 +1,29 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 class Solution{
 	public static int[] countBits(int n){
-		int[] output = new int[n];
-		for (int i = 0; i < n; i ++){
-			int value = n;
+		List<Integer> output = new ArrayList<>();
+		for (int i = 0; i <= n; i ++){
+			int value = i;
 			int count = 0;
-			while (value != 1){
+			while (value > 0){
 				if (value % 2 != 0){
 					count++;
 				}
 				value = value/ 2;
 			}
-			output.append(count);	
+			output.add(count);
+			System.out.println(output);	
 		}
-		result output;
+		int[] output_int = output.stream().mapToInt(Integer::intValue).toArray();
+		return output_int;
 	}
 
 	public static void main(String[] args){
 		int number = 5;
 		int [] output = new int[number];
 		output = countBits(number);
-		System.out.println(output);
+		System.out.println(Arrays.toString(output));
 	}
 }
